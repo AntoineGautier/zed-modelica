e3e03f0fcebc4edbe3993b7a00885a77428ed068
AntoineGautier <agautier@solamen.fr>
2025-12-27 18:24:16 +0100

    Remove ignored files from tracking

--- formatter/dist/cli.d.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-/**
- * Modelica Formatter CLI
- * Formats Modelica source files using Prettier with tree-sitter
- *
- * Usage: modelica-format <file.mo> [options]
- */
-export {};
-//# sourceMappingURL=cli.d.ts.map
